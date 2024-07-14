77667cf8576334ed2de5fc0bea4fa7d7f4d357a7
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-14 20:10:12 -0300

    Flyway migration v1

---
+++ src/main/resources/db_migration/V1__create_table_tb_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_user (
+    id text PRIMARY KEY UNIQUE NOT NULL ,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL
+);
